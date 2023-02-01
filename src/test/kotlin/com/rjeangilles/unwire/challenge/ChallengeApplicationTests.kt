90db13ff7d047df1d68d39e784de93d0dda3e552
Régis Jean-Gilles <rjean-gilles@users.noreply.github.com>
2023-02-01 09:26:16 +0100

    Commit project skeleton generated by Spring Initializr

---
+++ src/test/kotlin/com/rjeangilles/unwire/challenge/ChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rjeangilles.unwire.challenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
